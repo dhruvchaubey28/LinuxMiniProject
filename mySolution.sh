72ea622f918195f74df598d28e587a92bb04de34
Dhruv Chaubey <143328600+dhruvchaubey28@users.noreply.github.com>
2024-09-16 12:24:12 +0530

    Update mySolution.sh

--- mySolution.sh
+++ mySolution.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+cd src
+rm -rf maliciousFiles
+mkdir  secretDir
+cd secretDir
+touch .secret
+chmod 600 .secret
+SECRET=$(head -n 1 .secret)
+cd ..
+echo "$SECRET" | cut -c -32 > SOLUTION 
+chmod +x mySolution.sh
+./mySolution.sh
